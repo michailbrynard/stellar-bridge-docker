4b6e90fec2f2fa097d2a89b61e6b5e54424ebec5
Ramon Tayag <ramon.tayag@gmail.com>
2017-03-03 18:27:18 +0800

    Correct switch for custom config location

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = 'bridge' ]; then
   /app/templater.sh /app/bridge.cfg.template > /app/bridge.cfg
   bridge --migrate-db
-  bridge -config /app/bridge.cfg
+  bridge --config /app/bridge.cfg
 fi
 
 exec "$@"
